09bf805af314e974bc048fe631abd57ed876a196
鹏程Frank <38257014+milteau@users.noreply.github.com>
2023-08-22 09:33:09 +0800

    Add files via upload

---
+++ init.el
@@ -0,0 +1,66 @@
+;;; init.el --- The main entry for emacs -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, and then reset it by the
+;; `gcmh' package.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Increase how much is read from processes in a single chunk (default is 4kb).
+;; `lsp-mode' benefits from that.
+(setq read-process-output-max (* 4 1024 1024))
+
+(require 'package)
+(setq package-archives
+      '(("melpa"  . "https://melpa.org/packages/")
+        ("gnu"    . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
+(eval-when-compile
+  (require 'use-package))
+(use-package no-littering
+  :ensure t
+  :demand t)
+
+;; Bootstrap `quelpa'.
+(use-package quelpa
+  :ensure t
+  :commands quelpa
+  :custom
+  (quelpa-git-clone-depth 1)
+  (quelpa-self-upgrade-p nil)
+  (quelpa-update-melpa-p nil)
+  (quelpa-checkout-melpa-p nil))
+
+;; --debug-init implies `debug-on-error'.
+(setq debug-on-error init-file-debug)
+
+(let ((dir (locate-user-emacs-file "lisp")))
+  (add-to-list 'load-path (file-name-as-directory dir))
+  (add-to-list 'load-path (file-name-as-directory (expand-file-name "lang" dir))))
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+;; standalone apps
+(require 'init-org)
+(require 'init-base)
+(require 'init-dired)
+
+;; MacOS specific
+(when (eq system-type 'darwin)
+  (require 'init-osx))
+
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(provide 'init)
